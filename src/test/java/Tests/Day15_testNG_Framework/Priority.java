d09d502b6cbb3bf3645fe8c5c0b13ee5ec458914
HarunnMetin <mtnmusa07@gmail.com>
2024-03-20 16:39:59 +0300

    Initial commit

---
+++ src/test/java/Tests/Day15_testNG_Framework/Priority.java
@@ -0,0 +1,30 @@
+package Tests.Day15_testNG_Framework;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Priority {
+
+    int a =20;
+    int b = 10;
+
+    @Test (priority = 45)
+    public void carpmaTesti(){
+
+        //sayilarin carpiminin yuzden buyuk oldugunu test edin
+        Assert.assertTrue(a*b>100);
+    }
+    @Test (priority = 5)
+    public void toplamaTesti(){
+
+        //sayilartin toplaminin pozitif bir tamsayi oldugunu test edin
+
+        Assert.assertTrue(a+b>0);
+    }
+    @Test (priority = -9)
+    public void cikarmaTesti(){
+
+        //a ile b nin farkinin mutlak degerinin 100'den kucuk oldugunu test edin
+        Assert.assertTrue((a-b)>-100 && (a-b)<100);
+    }
+}
